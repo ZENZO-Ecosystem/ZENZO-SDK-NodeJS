a14ecced5f00de5da72e6fe2ef5ca30b995f0ff3
JSKitty <loopylew113@protonmail.com>
2019-05-10 13:27:46 +0100

    Update ping.js

--- examples/ping.js
+++ examples/ping.js
@@ -1,6 +1,6 @@
 var ZENZO = require("../index.js")
 
-ZENZO.auth("6d02111e06b1a780ac1821bb11a7edab715e0457ca613bd7267dd4432a7c8553")
+ZENZO.auth("insert-zenzo-api-key")
 
 ZENZO.ping().then(res => {
   console.log(res)
